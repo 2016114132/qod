83cd8bd4288bda7459deec9c3b3bb3b90f55f2b6
Dair Aban <2016114132@ub.edu.bz>
2025-08-25 14:11:42 -0600

    Update

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,14 +3,63 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log/slog"
+	"os"
 )
 
+type configuration struct {
+	port int
+	env  string
+	vrs  string
+}
+type application struct {
+	config configuration
+	logger *slog.Logger
+}
+
 func printUB() string {
-    return "Hello, UB!"
+	return "Hello, UB!"
 }
 
 func main() {
-    greeting := printUB()
-    fmt.Println(greeting)
+	greeting := printUB()
+	fmt.Println(greeting)
+
+	//Initialize configuration
+	cfg := loadConfig()
+	//Initialize logger
+	logger := setupLogger()
+	//Initialize applicatioin with dependencies
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// Start the application server
+	if err := app.serve(); err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+}
+
+func loadConfig() configuration {
+	var cfg configuration
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	flag.StringVar(&cfg.vrs, "version", "1.0.0", "Application version")
+	flag.Parse()
+
+	return cfg
+}
+
+func setupLogger() *slog.Logger {
+	var logger *slog.Logger
+
+	logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	return logger
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/2016114132/qod
 
 go 1.24.5
+
+require github.com/julienschmidt/httprouter v1.3.0 
