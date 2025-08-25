07ba108d9237d70c1a953179f028590d50736e0e
Dair Aban <2016114132@ub.edu.bz>
2025-08-25 14:38:35 -0600

    Encoding json v2

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,18 +1,29 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter,
 	r *http.Request) {
 
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, app.config.vrs)
-	// Content-Type is text/plain by default
-	w.Header().Set("Content-Type", "application/json")
-	// Write the JSON as the HTTP response body.
-	w.Write([]byte(js))
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     app.config.vrs,
+	}
 
+	jsResponse, err := json.Marshal(data)
+
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	jsResponse = append(jsResponse, '\n')
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsResponse)
 }
