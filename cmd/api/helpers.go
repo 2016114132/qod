172e153b11dca541c7a0f63f32d142173faeb0d4
Dair Aban <2016114132@ub.edu.bz>
2025-09-03 10:19:54 -0600

    Update to restricting the input

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
 type envelope map[string]any
@@ -40,27 +41,51 @@ func (a *application) readJSON(w http.ResponseWriter,
 	r *http.Request,
 	destination any) error {
 
-	err := json.NewDecoder(r.Body).Decode(destination)
+	// what is the max size of the request body (250KB seems reasonable)
+	maxBytes := 256_000
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	// our decoder will check for unknown fields
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	// let start the decoding
+	err := dec.Decode(destination)
+
+	// err := json.NewDecoder(r.Body).Decode(destination)
 	if err != nil {
 		// check for the different errors
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
 		var invalidUnmarshalError *json.InvalidUnmarshalError
+		var maxBytesError *http.MaxBytesError
 
 		switch {
 		case errors.As(err, &syntaxError):
 			return fmt.Errorf("the body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
 			// Decode can also send back an io error message
 		case errors.Is(err, io.ErrUnexpectedEOF):
 			return errors.New("the body contains badly-formed JSON")
 
 		case errors.As(err, &unmarshalTypeError):
 			if unmarshalTypeError.Field != "" {
-				return fmt.Errorf("the body contains the incorrect JSON type for field %q", unmarshalTypeError.Field)
+				return fmt.Errorf("the body contains the incorrect JSON type for field %q",
+					unmarshalTypeError.Field)
 			}
-			return fmt.Errorf("the body contains the incorrect  JSON type (at character %d)", unmarshalTypeError.Offset)
+			return fmt.Errorf("the body contains the incorrect  JSON type (at character %d)",
+				unmarshalTypeError.Offset)
+
 		case errors.Is(err, io.EOF):
 			return errors.New("the body must not be empty")
+
+			// check for unknown field error
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(),
+				"json: unknown field ")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+
+		// does the body exceed our limit of 250KB?
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("the body must not be larger than %d bytes", maxBytesError.Limit)
 
 		// the programmer messed up
 		case errors.As(err, &invalidUnmarshalError):
@@ -69,6 +94,17 @@ func (a *application) readJSON(w http.ResponseWriter,
 		default:
 			return err
 		}
+	}
+
+	//Let's lastly check if there is any data after
+	// the valid JSON data. Maybe the person is trying to send
+	// multiple request bodies during one request
+	// We call decode once more to see if it gives us back anything
+	// we use a throw away struct 'struct{}{}' to hold the result
+	err = dec.Decode(&struct{}{})
+
+	if !errors.Is(err, io.EOF) { // there is more data present
+		return errors.New("the body must only contain a single JSON value")
 	}
 
 	return nil
