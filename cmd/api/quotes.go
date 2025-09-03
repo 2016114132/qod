939cf16e67ef491e78fd400ca4b660f007c4a9cf
Dair Aban <2016114132@ub.edu.bz>
2025-09-03 09:49:29 -0600

    implemented function to read json and check for error while decoding

--- cmd/api/quotes.go
+++ cmd/api/quotes.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	// import the data package which contains the definition for Quote
@@ -17,9 +16,11 @@ func (a *application) createQuoteHandler(w http.ResponseWriter,
 		Author  string `json:"author"`
 	}
 	// perform the decoding
-	err := json.NewDecoder(r.Body).Decode(&incomingData)
+	// err := json.NewDecoder(r.Body).Decode(&incomingData)
+	err := a.readJSON(w, r, &incomingData)
 	if err != nil {
-		a.errorResponseJSON(w, r, http.StatusBadRequest, err.Error())
+		// a.errorResponseJSON(w, r, http.StatusBadRequest, err.Error())
+		a.badRequestResponse(w, r, err)
 		return
 	}
 
