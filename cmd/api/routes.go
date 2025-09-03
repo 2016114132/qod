97975abced056835d2cf748a3d8685b3342045cb
Dair Aban <2016114132@ub.edu.bz>
2025-09-03 09:38:47 -0600

    added quote route

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,6 +15,7 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 	// setup routes
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/quotes", app.createQuoteHandler)
 
 	return app.recoverPanic(router)
 }
