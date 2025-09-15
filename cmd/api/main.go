a049257409298389afc7239ff743dcbf56be4e29
Dair Aban <2016114132@ub.edu.bz>
2025-09-15 00:04:53 -0600

    Added connection pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,16 +3,24 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
 type configuration struct {
 	port int
 	env  string
 	vrs  string
+	db   struct {
+		dsn string
+	}
 }
 type application struct {
 	config configuration
@@ -31,6 +39,18 @@ func main() {
 	cfg := loadConfig()
 	//Initialize logger
 	logger := setupLogger()
+
+	// the call to openDB() sets up our connection pool
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	// release the database resources before exiting
+	defer db.Close()
+
+	logger.Info("database connection pool established")
+
 	//Initialize applicatioin with dependencies
 	app := &application{
 		config: cfg,
@@ -51,6 +71,8 @@ func loadConfig() configuration {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
 	flag.StringVar(&cfg.vrs, "version", "1.0.0", "Application version")
+	// read in the dsn
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://developer:root@localhost/quotes?sslmode=disable", "PostgreSQL DSN")
 	flag.Parse()
 
 	return cfg
@@ -62,4 +84,27 @@ func setupLogger() *slog.Logger {
 	logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	return logger
+}
+
+func openDB(settings configuration) (*sql.DB, error) {
+	// open a connection pool
+	db, err := sql.Open("postgres", settings.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// set a context to ensure DB operations don't take too long
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	// let's test if the connection pool was created
+	// we trying pinging it with a 5-second timeout
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	// return the connection pool (sql.DB)
+	return db, nil
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/2016114132/qod
 
 go 1.24.5
 
-require github.com/julienschmidt/httprouter v1.3.0 
+require github.com/julienschmidt/httprouter v1.3.0
+
+require github.com/lib/pq v1.10.9
